9769bb1e90a727475118b7341c29cbf4eac51045
raselahmed825 <raelahmed825@gmail.com>
2017-10-04 09:17:01 +0600

    Assignment6

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,13 @@
 //JS code goes here
+//first method
+var turn = false;
+function play(event){
+console.log(event);
+event.target.innerHTML = turn?"O" : "X";
+turn = !turn;
+}
+
+//second method
+// var cell0= window.document.getElementById("0");
+//
+// cell0.addEventListener('click',play);
